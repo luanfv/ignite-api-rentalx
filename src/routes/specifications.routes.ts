fd889db8bee3b272a6535204d1c20e77af081832
luanfv <luanfrancavieira@gmail.com>
2022-07-20 16:17:31 -0300

    refactor: routes

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,11 @@
-/* eslint-disable prettier/prettier */
 import { Router } from 'express';
 
 import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
 
-specificationsRoutes.post('/', (request, response) => createSpecificationController.handle(request, response));
+specificationsRoutes.post('/', (request, response) =>
+  // eslint-disable-next-line implicit-arrow-linebreak, prettier/prettier
+  createSpecificationController.handle(request, response));
 
 export { specificationsRoutes };
